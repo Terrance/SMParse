b7949a2788a7ac97aa2787919195cfea3bb1ba88
Ollie Terrance <ollie.terrance@live.co.uk>
2016-02-23 20:29:41 +0000

    Rewrite render for HTML5 canvas

--- SMParse/res/js/parser.js
+++ SMParse/res/js/parser.js
@@ -11,6 +11,27 @@ var SMParse = {
         "L": "lift",
         "F": "fake"
     },
+    colour: function colour(beat, size) {
+        var divs = 3 * 128;
+        var val = beat * divs / size;
+        if (val % (divs / 4) === 0) {
+            return 0;
+        } else if (val % (divs / 8) === 0) {
+            return 1;
+        } else if (val % (divs / 12) === 0) {
+            return 2;
+        } else if (val % (divs / 16) === 0) {
+            return 3;
+        } else if (val % (divs / 24) === 0) {
+            return 4;
+        } else if (val % (divs / 32) === 0) {
+            return 5;
+        } else if (val % (divs / 64) === 0) {
+            return 6;
+        } else {
+            return 7;
+        }
+    },
     parse: function parse(str) {
         var lines = str.replace(/\/\/.*$/gm, "").split(";");
         var data = {};
@@ -46,15 +67,15 @@ var SMParse = {
         }
         data["RADARVALUES"] = data["RADARVALUES"].split(",");
         var notes = data["NOTES"].trim().split(/,\s*/);
-        var beats = [];
+        var bars = [];
         for (var i in notes) {
-            var beatNotes = notes[i].trim().replace(/\s+/gm, " ").split(/\s+/);
-            for (var j in beatNotes) {
-                var beat = 4 * (parseInt(i) + (parseInt(j) / beatNotes.length));
-                beats.push([beat].concat(beatNotes[j].split("")));
+            var barNotes = notes[i].trim().replace(/\s+/gm, " ").split(/\s+/);
+            for (var j in barNotes) {
+                barNotes[j] = barNotes[j].split("");
             }
+            bars.push([parseInt(i), barNotes]);
         }
-        data["NOTES"] = beats;
+        data["NOTES"] = bars;
         return data;
     }
 };
--- SMParse/res/js/ui.js
+++ SMParse/res/js/ui.js
@@ -1,57 +1,110 @@
 "use strict";
 $(document).ready(function() {
     var arrows = ["<", "V", "\u039b", ">"];
-    function pad(str, len) {
-        while (str.length < len) str = " " + str;
-        return str;
-    }
-    function render(data) {
-        var lines = [];
-        var last = -1;
+    var rotates = [90, 0, 180, 270];
+    var size = 64, zoom = 4;
+    function render($canvas, data) {
         var holds = [false, false, false, false];
+        var end = data["NOTES"][data["NOTES"].length - 1][0] + 1;
+        var margin = ($canvas.width() - (4 * size)) / 2;
+        $canvas.clearCanvas().prop("height", Math.min(32767, (end * size * zoom) + size));
+        for (var i = 0; i <= end; i++) {
+            $canvas.drawLine({
+                strokeStyle: "#333",
+                strokeWidth: 2,
+                x1: 0,
+                y1: (i * size * zoom) + (size / 2),
+                x2: 512,
+                y2: (i * size * zoom) + (size / 2)
+            }).drawLine({
+                strokeStyle: "#999",
+                strokeWidth: 2,
+                x1: 0,
+                y1: ((i - 0.5) * size * zoom) + (size / 2),
+                x2: 512,
+                y2: ((i - 0.5) * size * zoom) + (size / 2)
+            }).drawLine({
+                strokeStyle: "#ccc",
+                strokeWidth: 2,
+                x1: 0,
+                y1: ((i - 0.25) * size * zoom) + (size / 2),
+                x2: 512,
+                y2: ((i - 0.25) * size * zoom) + (size / 2)
+            }).drawLine({
+                strokeStyle: "#ccc",
+                strokeWidth: 2,
+                x1: 0,
+                y1: ((i - 0.75) * size * zoom) + (size / 2),
+                x2: 512,
+                y2: ((i - 0.75) * size * zoom) + (size / 2)
+            });
+        }
         for (var i in data["NOTES"]) {
-            var notes = data["NOTES"][i];
-            var beat = notes[0];
-            var bar = Math.floor(beat / 4);
-            var steps = notes.slice(1);
-            var line = (bar > last ? pad(bar.toString(), 4) : "    ") + " ";
-            var space = (bar > last ? "-" : " ");
-            for (var j in steps) {
-                line += space;
-                switch (steps[j]) {
-                    case "0":
-                        line += (holds[j] || space);
-                        break;
-                    case "1":
-                        line += arrows[j];
-                        break;
-                    case "2":
-                        holds[j] = "|";
-                        line += arrows[j];
-                        break;
-                    case "4":
-                        holds[j] = "\u00a6";
-                        line += arrows[j];
-                        break;
-                    case "3":
-                        holds[j] = false;
-                        line += "+";
-                        break;
-                    default:
-                        line += steps[j];
-                        break;
+            var bar = data["NOTES"][i][0];
+            var notes = data["NOTES"][i][1];
+            for (var j = 0; j < notes.length; j++) {
+                for (var k in notes[j]) {
+                    var step = notes[j][k];
+                    switch (step) {
+                        case "1": // step
+                            $canvas.drawImage({
+                                source: "res/img/notes.png",
+                                x: (k * size) + margin,
+                                y: (bar + (j / notes.length)) * size * zoom,
+                                width: size,
+                                height: size,
+                                fromCenter: false,
+                                sx: 0,
+                                sy: (SMParse.colour(j, notes.length) + 0.5) * 128,
+                                sWidth: 128,
+                                sHeight: 128,
+                                cropFromCenter: false,
+                                rotate: rotates[k]
+                            });
+                            break;
+                        case "2": // hold start
+                        case "4": // roll start
+                            holds[k] = [bar, j, notes.length];
+                            break;
+                        case "3": // hold/roll stop
+                            var hBar = holds[k][0];
+                            var hJ = holds[k][1];
+                            var hNotesLength = holds[k][2];
+                            var hPos = hBar + (hJ / hNotesLength);
+                            $canvas.drawImage({
+                                source: "res/img/hold.png",
+                                x: (k * size) + margin,
+                                y: (hPos * size * zoom) + (size / 2),
+                                width: size,
+                                height: (bar + (j / notes.length) - hPos) * size * zoom,
+                                fromCenter: false
+                            }).drawImage({
+                                source: "res/img/hold-end.png",
+                                x: (k * size) + margin,
+                                y: ((bar + (j / notes.length)) * size * zoom) + (size / 2),
+                                width: size,
+                                height: size / 2,
+                                fromCenter: false
+                            }).drawImage({
+                                source: "res/img/notes.png",
+                                x: (k * size) + margin,
+                                y: hPos * size * zoom,
+                                width: size,
+                                height: size,
+                                fromCenter: false,
+                                sx: 0,
+                                sy: (SMParse.colour(hJ, hNotesLength) + 0.5) * 128,
+                                sWidth: 128,
+                                sHeight: 128,
+                                cropFromCenter: false,
+                                rotate: rotates[k]
+                            });
+                            holds[k] = false;
+                            break;
+                    }
                 }
             }
-            lines.push(line + space);
-            last = bar;
         }
-        return lines.join("\n");
-    }
-    function parse(str) {
-        var data = SMParse.parse(str);
-        console.log(data);
-        $("#data").val(JSON.stringify(data, null, 2));
-        $("#render").val(render(data));
     }
     $(window).on("dragover", function(e) {
         e.stopPropagation();
@@ -64,9 +117,14 @@ $(document).ready(function() {
         e.preventDefault();
         var reader = new FileReader();
         reader.onload = function(e) {
-            parse(e.target.result);
+            var data = SMParse.parse(e.target.result);
+            console.log(data);
+            $("#data").val(JSON.stringify(data, null, 2));
+            render($("#render canvas"), data);
         };
         document.title = files[0].name + " -- StepMania chart parser";
         reader.readAsText(files[0]);
     });
+    var imgs = ["notes", "hold", "hold-end"];
+    for (var i in imgs) (new Image()).src = "res/img/" + imgs[i] + ".png";
 });
